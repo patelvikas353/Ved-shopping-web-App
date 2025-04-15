02588adff00a09a72092e8a8c7433e498cd37688
e <vp8081628@gmail.com>
2025-04-15 09:18:18 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import {ShopContextProvider} from './context/ShopContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ShopContextProvider>

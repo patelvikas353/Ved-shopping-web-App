58ca611ea280c3e801d0e681c03189a9ca8a9b72
e <vp8081628@gmail.com>
2025-04-22 11:56:53 +0530

    fix

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import { ShopContext } from '../context/shopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
 const BestSeller = () => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './context/shopContext.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ShopContextProvider>

9ddd08f75361281b614d9cb6d6c365a9dacf8db7
e <vp8081628@gmail.com>
2025-04-22 11:50:05 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import ShopContextProvider from './context/shopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ShopContextProvider>
     <App/>
   </ShopContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
